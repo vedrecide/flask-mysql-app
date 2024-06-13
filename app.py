42c4a0e0caf1a2a6dc22322f77e9a7a1e71abcb1
Mohammed Rayyan <mohammedrayyan112233@gmail.com>
2024-06-13 21:51:53 +0530

    minor changes

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bcrypt import Bcrypt
 
 import json
 import mysql.connector
 from mysql.connector import errorcode
+from uuid import uuid4
 
 
 with open("./config.json", "r") as f:
@@ -17,6 +19,7 @@ try:
     port=config["DATABASE_PORT"], 
     database=config["DATABASE_NAME"]
   )
+  cur = conn.cursor()
   print("Connection established successfully", flush=True)
 except mysql.connector.Error as err:
   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -25,13 +28,17 @@ except mysql.connector.Error as err:
     print("Database does not exist", flush=True)
   else:
     print(err, flush=True)
-else:
-  conn.close()
 
+with app.app_context():
+  cur.execute("CREATE TABLE IF NOT EXISTS User(id CHAR(36) PRIMARY KEY, username VARCHAR(30) NOT NULL, password BINARY(60) NOT NULL, teacher TINYINT)")
 
 @app.route("/")
 def home():
-  return "Home"
+  return render_template("index.html")
+
+@app.route("/questions")
+def questions():
+  return render_template("questions.html")
 
 if __name__ == "__main__":
   app.run(debug=bool(config["DEBUG"]))
